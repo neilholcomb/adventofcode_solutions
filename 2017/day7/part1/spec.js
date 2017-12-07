4886f8144dc4619041e18a0d758eb1536a080f6e
Neil Holcomb <neil@holcomb.ws>
2017-12-07 21:13:13 +0800

    2017 day 7 part 1 solution

---
+++ 2017/day7/part1/spec.js
@@ -0,0 +1,23 @@
+import path from 'path'
+import * as solutions from './solution'
+
+//get absolute path to our input
+let testinput = path.join(__dirname, 'test.txt')
+let testsolution = 'tknk'
+
+let inputPath = path.join(__dirname, 'input.txt')
+let solution = 'cyrupz'
+
+describe('2017 Day 7 part 1 solutions', () => {
+  it('should work with solution 1', () => {
+    expect(solutions.solution1(inputPath)).toEqual(solution)
+  })
+
+  it('should work with solution 1', () => {
+    expect(solutions.solution1(testinput)).toEqual(testsolution)
+  })
+
+  // it('should work with golf solution', () => {
+  //   expect(solutions.golf([...input])).toEqual(solution)
+  // })
+})
